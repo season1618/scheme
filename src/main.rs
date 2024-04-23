7aa134ad9a6755c16bf6b8fd8908c2bf087da854
season1618 <seasonminority314@gmail.com>
2024-04-23 14:48:41 +0900

    interpreter

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,15 @@ use crate::eval::exec;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let code = fs::read_to_string(&args[1]).expect("file not found");
-    let tokens = match tokenize(&code) {
-        Ok(tokens) => tokens,
-        Err(err) => {
-            eprintln!("{}", err);
-            return;
-        },
-    };
-    let nodes = match parse(tokens) {
-        Ok(nodes) => nodes,
-        Err(err) => {
-            eprintln!("{}", err);
-            return;
-        }
-    };
-    match exec(nodes) {
+    match interprete(&args[1]) {
         Ok(()) => {},
-        Err(err) => {
-            eprintln!("{}", err);
-            return;
-        },
+        Err(err) => eprintln!("{err}"),
     }
+}
+
+fn interprete(file: &str) -> Result<(), String> {
+    let code = &fs::read_to_string(file).expect("file not found");
+    let tokens = tokenize(code)?;
+    let nodes = parse(tokens)?;
+    exec(nodes)
 }
