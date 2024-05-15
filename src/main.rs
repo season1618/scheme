37ebb477cc57e6b4cd1bf98d363ed1114650c0d9
season1618 <seasonminority314@gmail.com>
2024-05-15 23:07:45 +0900

    repl

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,21 @@ mod parser;
 mod exec;
 
 use esp32_hal::{
+    peripherals::UART0,
     prelude::entry,
+    uart::Uart,
 };
-use esp_println::println;
+use esp_println::{print, println};
 use alloc::{
     string::String,
 };
 
 use crate::m5core2::{m5core2_new, read_line};
 use crate::embedded::init_heap;
+use crate::data::Env;
 use crate::lexer::tokenize;
 use crate::parser::parse;
-use crate::exec::exec;
+use crate::exec::{exec, exec_line};
 
 #[entry]
 fn main() -> ! {
@@ -32,10 +35,35 @@ fn main() -> ! {
 
     interprete().unwrap();
 
-    let buf: &mut [u8] = &mut [0; 128];
     loop {
-        let msg = read_line(&mut uart, buf).unwrap();
-        println!("{}", msg);
+        repl(&mut uart).unwrap();
+    }
+}
+
+fn repl<'a>(uart: &mut Uart<'a, UART0>) -> Result<(), String> {
+    let buf: &mut [u8] = &mut [0; 128];
+    let mut env = Env::new();
+    loop {
+        print!("> ");
+
+        match read_line(uart, buf) {
+            Ok("") => break Ok(()),
+            Ok(code) => {
+                let tokens = tokenize(&code)?;
+                if tokens.len() == 0 {
+                    continue;
+                } else {
+                    let nodes = parse(tokens)?;
+                    for node in nodes {
+                        match exec_line(node, &mut env) {
+                            Ok(()) => {},
+                            Err(err) => println!("{err}"),
+                        }
+                    }
+                }
+            },
+            Err(err) => println!("{err}"),
+        }
     }
 }
 
