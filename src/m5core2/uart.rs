831e71e3eacaf661b7bf2132fee277f2b1c9de7d
season1618 <seasonminority314@gmail.com>
2024-05-18 05:10:50 +0900

    backspace

--- src/m5core2/uart.rs
+++ src/m5core2/uart.rs
@@ -5,15 +5,22 @@ use crate::m5core2::M5Core2;
 
 impl<'a> M5Core2<'_> {
     pub fn read_line<'b>(&mut self, buf: &'b mut [u8]) -> Result<&'b str, Utf8Error> {
-        for idx in 0..buf.len() {
-            let _ = self.read_u8();
+        let mut idx = 0;
+        while idx < buf.len() {
+            let _ = self.read_u8(); // espflash monitor sends input twice.
             let c = self.read_u8();
-            if c == b'\r' {
+            if c == b'\r' { // espflash monitor sends CR as new line.
                 self.uart.write(b'\n').unwrap();
                 return from_utf8(&buf[..idx]);
+            } else if c == 0x08 { // backspace
+                if idx > 0 {
+                    self.uart.write(0x08).unwrap();
+                    idx -= 1;
+                }
             } else {
                 self.uart.write(c).unwrap();
                 buf[idx] = c;
+                idx += 1;
             }
         }
         from_utf8(buf)
